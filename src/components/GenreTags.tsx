359c44d995bc113323aeb8942c945fe0522a1adc
Amna <akram.aamnah@gmail.com>
2023-12-22 12:58:10 +0200

    cleanup

--- src/components/GenreTags.tsx
+++ src/components/GenreTags.tsx
@@ -1,28 +1,24 @@
 import './GenreTags.scss'
 
-interface GenreTagsProps {
+interface Props {
   genres: string[]
 }
-export default function GenreTags({ genres }: GenreTagsProps) {
-  // let genres = useContext(AppContext)
 
-  function renderGenres(genresArray: string[]) {
-    // console.log(`movieGenres inside GenerateTags`, genresArray)
+function renderGenres(genresArray: string[]) {
+  const genreTags = genresArray.map((genre, index) => {
+    return (
+      <span
+        className="GenreTagItem flex justify-center content-center items-center border rounded-lg py-1 px-2"
+        key={index}
+      >
+        <small>{genre}</small>
+      </span>
+    )
+  })
+  return genreTags
+}
 
-    const genreTags = genresArray.map((genre, index) => {
-      // console.log(`Genre Name:`, genre)
-      // console.log(genresArray[genre])
-      return (
-        <span
-          className="GenreTagItem flex justify-center content-center items-center border rounded-lg py-1 px-2"
-          key={index}
-        >
-          <small>{genre}</small>
-        </span>
-      )
-    })
-    return genreTags
-  }
+export default function GenreTags({ genres }: Props) {
   return (
     <div className="flex flex-row flex-wrap gap-2 mt-2">
       {renderGenres(genres)}
