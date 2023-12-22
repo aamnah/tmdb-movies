93727db8cd3f442515da1ce53aab8c2c1a03c8d0
Amna <akram.aamnah@gmail.com>
2023-12-22 14:33:56 +0200

    + response interfaces

--- src/api/tmdb_api.d.ts
+++ src/api/tmdb_api.d.ts
@@ -22,6 +22,17 @@ export interface Genre {
   name: string
 }
 
+export interface GenreList {
+  genres: Genre[]
+}
+
+export interface Collection {
+  id: number
+  name: string
+  poster_path: string
+  backdrop_path: string
+}
+
 export interface Video {
   id: string
   iso_639_1: string
@@ -35,13 +46,65 @@ export interface Video {
   type: string
 }
 
+export interface Videos extends Video {
+  id: number
+  results: Video[]
+}
 export interface MovieList {
   dates: {
-    maximum: string
-    minimum: string
+    maximum: Date
+    minimum: Date
   }
   page: number
   results: Movie[]
   total_pages: number
   total_results: number
+}
+
+export interface Company {
+  id: number
+  logo_path: string
+  name: string
+  origin_country: string
+}
+
+export interface Country {
+  iso_3166_1: string
+  name: string
+}
+
+export interface Language {
+  english_name: string
+  iso_639_1: string
+  name: string
+}
+
+// https://api.themoviedb.org/3/movie/{movie_id}
+export interface MovieDetail extends Movie {
+  belongs_to_collection: null | Collection
+  budget: number
+  genres: Genre[]
+  homepage: string
+  imdb_id: string
+  production_companies: Company[]
+  production_countries: Country[]
+  revenue: number
+  runtime: number
+  spoken_languages: Language[]
+  status: string // Released
+  tagline: string
+  videos: {
+    results: Video[]
+  }
+  images: { backdrops: []; logos: []; posters: [] }
+}
+
+export interface CollectionPart extends Movie {
+  media_type: string // movie
+}
+
+// https://api.themoviedb.org/3/collection/{collection_id}
+export interface CollectionDetail extends Collection {
+  overview: string
+  parts: CollectionPart[]
 }
