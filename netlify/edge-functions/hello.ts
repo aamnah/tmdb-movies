728478c3483a909988add14bea8eaae6e0c058ea
Amna <akram.aamnah@gmail.com>
2023-12-22 20:15:43 +0200

    rename, remove redundant

--- netlify/edge-functions/hello.ts
+++
@@ -1,9 +0,0 @@
-import type { Config, Context } from '@netlify/edge-functions'
-
-export default async function Hello(request: Request, context: Context) {
-  return new Response('Hello world')
-}
-
-export const config: Config = { path: '/test' }
-
-
