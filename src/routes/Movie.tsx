14cb1ddad9c31d69c3c36247055ff86a3d4853e6
Amna <akram.aamnah@gmail.com>
2023-12-22 15:51:51 +0200

    + Movie page layout

--- src/routes/Movie.tsx
+++ src/routes/Movie.tsx
@@ -1,17 +1,34 @@
-import { useLoaderData } from 'react-router-dom'
+import { Params, useLoaderData } from 'react-router-dom'
 import { fetchMovieDetails } from '../api/api'
+import GenreTags from '../components/GenreTags'
+import { getGenreNames, getPosterURL } from '../helpers'
+import { MovieDetail, poster_sizes } from '../api/tmdb_api.types'
 
 export default function Movie() {
   const movie = useLoaderData()
 
   return (
     <>
-      <h1>{movie.title}</h1>
+      <h1 className="text-3xl font-semibold">{movie.title}</h1>
+      <p>{movie.release_date}</p>
+      <div className="grid grid-cols-[1fr_2fr]">
+        <img
+          src={getPosterURL({
+            path: movie.poster_path,
+            width: poster_sizes.w500,
+          })}
+          alt={movie.title}
+          className="w-full rounded-2xl"
+        />
+      </div>
+      <GenreTags genres={getGenreNames(movie.genres)} />
+      <p className="text-xl">{movie.tagline}</p>
+      <p>{movie.overview}</p>
     </>
   )
 }
 
-export async function loader({ params }) {
-  const movie = fetchMovieDetails(params.movieId)
+export async function loader({ params }: Params) {
+  const movie = await fetchMovieDetails(params.movieId)
   return movie
 }
